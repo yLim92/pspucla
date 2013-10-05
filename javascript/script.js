2002603ef3ade33ec01aaf767dbf4865a541b3ee
U-Lazer\Yongjie <Yongjie@Lazer.(none)>
2013-10-05 14:03:44 -0700

    minor changes to header, started rush page

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,19 @@
 $(function() {
-	console.log('hola');
-}
+	var url = window.location.pathname;
+	var template = url.substring(url.lastIndexOf('/') + 1);
+	if (template.length > 0){
+		$("#navigation #"+ template).css({
+			backgroundColor: "gold"
+		});
+	}
+	else {
+		$("#navigation #home").css({
+			backgroundColor: "gold"
+		});
+	}
+	if (template == "rush")
+		$('#header').addClass('inverted');
+	$('#header').css({
+		visibility: 'visible'
+	});
+});
